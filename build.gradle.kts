284bc666415b3317b7134aa578c63c04bcdb76da
José Vitor <ext.jose.silva@gympass.com>
2024-03-07 10:28:01 -0300

    build

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -91,3 +92,5 @@ buildscript {
 		classpath("org.springframework.boot:spring-boot-gradle-plugin:2.5.5")
 	}
 }
+
+
